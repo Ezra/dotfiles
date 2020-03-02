6f06160400cb186ee0e11da1c320dbbe7485af69
Ezra Bradford <ezra.bradford@gmail.com>
2020-03-02 15:14:50 -0800

    update jupyter config template

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -252,21 +252,24 @@
 #  use by the buffer manager.
 #c.NotebookApp.max_buffer_size = 536870912
 
+## Gets or sets a lower bound on the open file handles process resource limit.
+#  This may need to be increased if you run into an OSError: [Errno 24] Too many
+#  open files. This is not applicable when running on Windows.
+#c.NotebookApp.min_open_files_limit = 0
+
 ## Dict of Python modules to load as notebook server extensions.Entry values can
 #  be used to enable and disable the loading ofthe extensions. The extensions
 #  will be loaded in alphabetical order.
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-#c.NotebookApp.notebook_dir = '/home/ezrab/notebooks'
+#c.NotebookApp.notebook_dir = ''
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
 #  module, unless it is overridden using the --browser (NotebookApp.browser)
 #  configuration option.
 c.NotebookApp.open_browser = True
-# don't use a file redirect, since the filesystems disagree
-c.NotebookApp.use_redirect_file = False
 
 ## Hashed password to use for web authentication.
 #  
@@ -349,6 +352,22 @@ c.NotebookApp.use_redirect_file = False
 #  For headerssent by the upstream reverse proxy. Necessary if the proxy handles
 #  SSL
 #c.NotebookApp.trust_xheaders = False
+
+## Disable launching browser by redirect file
+#  
+#  For versions of notebook > 5.7.2, a security feature measure was added that
+#  prevented the authentication token used to launch the browser from being
+#  visible. This feature makes it difficult for other users on a multi-user
+#  system from running code in your Jupyter session as you.
+#  
+#  However, some environments (like Windows Subsystem for Linux (WSL) and
+#  Chromebooks), launching a browser using a redirect file can lead the browser
+#  failing to load.  This is because of the difference in file structures/paths
+#  between the runtime and  the browser.
+#  
+#  Disabling this setting to False will disable this behavior, allowing the
+#  browser  to launch by using a URL and visible token (as before).
+c.NotebookApp.use_redirect_file = False
 
 ## DEPRECATED, use tornado_settings
 #c.NotebookApp.webapp_settings = {}
